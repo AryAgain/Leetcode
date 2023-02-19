dc5aab5a97973b0ab00dc9d0782aa2e0ad9d7497
Aryan <arysin163@gmail.com>
2023-02-19 01:52:33 -0500

    13 feb in progress

---
+++ 338_counting_bits.py
@@ -0,0 +1,12 @@
+# in progress
+
+
+def decimalToBinary(n):
+	return bin(n).replace("0b","")
+
+
+
+if __name__ == '__main__':
+	print(type(decimalToBinary(8)))
+	print(decimalToBinary(18))
+	print(decimalToBinary(7))
